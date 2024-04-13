74c562ae05f3c787a6ee582dccfdfa636850c830
muneebhashone <muneeb.hashone@gmail.com>
2024-04-13 05:31:52 +0500

    replace class with className

--- src/render.tsx
+++ src/render.tsx
@@ -8,17 +8,17 @@ export interface IPost {
 
 export const Post: FC<{ post: IPost }> = ({ post }) => {
   return (
-    <li class="shadow-sm my-2 p-4 rounded-md border border-slate-200 w-1/4">
+    <li className="shadow-sm my-2 p-4 rounded-md border border-slate-200 w-1/4">
       <h2>
-        <span class="text-xs text-slate-600 mr-2">TITLE:</span>
+        <span className="text-xs text-slate-600 mr-2">TITLE:</span>
         {post.title}
       </h2>
       <p>
-        <span class="text-xs text-slate-600 mr-2">DESCRIPTION:</span>
+        <span className="text-xs text-slate-600 mr-2">DESCRIPTION:</span>
         {post.description}
       </p>
       <button
-        class="border mt-2 text-sm p-2 hover:bg-blue-300"
+        className="border mt-2 text-sm p-2 hover:bg-blue-300"
         hx-target="closest li"
         hx-swap="outerHTML swap:1s"
         hx-delete={`/post/${post.id}`}
@@ -37,7 +37,7 @@ export const Layout: FC = (props) => {
         <link rel="stylesheet" href="/static/css/common.css" />
       </head>
       <body>
-        <main class="p-6">{props.children}</main>
+        <main className="p-6">{props.children}</main>
         <script src="https://unpkg.com/htmx.org@1.9.11"></script>
         <script src="/static/js/app.js"></script>
       </body>
@@ -48,23 +48,27 @@ export const Layout: FC = (props) => {
 export const Top: FC<{ posts: IPost[] }> = ({ posts }) => {
   return (
     <Layout>
-      <h1 class="text-3xl capitalize mb-4">Blogs</h1>
+      <h1 className="text-3xl capitalize mb-4">Blogs</h1>
       <form
         hx-post="/post"
         hx-target=".post-list"
         hx-swap="beforeend"
-        class="flex gap-2"
+        className="flex gap-2"
         {...{ "hx-on::after-request": "this.reset()" }}
       >
-        <input class="border shadow-sm p-2" placeholder="Title" name="title" />
         <input
-          class="border shadow-sm p-2"
+          className="border shadow-sm p-2"
+          placeholder="Title"
+          name="title"
+        />
+        <input
+          className="border shadow-sm p-2"
           placeholder="Description"
           name="description"
         />
         <button type="submit">Post</button>
       </form>
-      <ul class="post-list flex gap-2 w-screen flex-wrap">
+      <ul className="post-list flex gap-2 w-screen flex-wrap">
         {posts.map((post) => {
           return <Post post={post} />;
         })}
